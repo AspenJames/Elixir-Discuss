e7563a526988add8219cd50dc244d3d87d52a8c0
Aspen James <aspenjames@tqca.org>
2018-11-11 15:57:44 -0800

    remove unused files, create index, redirect after topic creation

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,6 +3,11 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
@@ -12,7 +17,10 @@ defmodule Discuss.TopicController do
   def create(conn, %{"topic" => topic}) do
     case Topic.changeset(%Topic{}, topic)
     |> Repo.insert do
-      {:ok, post} -> IO.inspect post
+      {:ok, post} -> 
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
       {:error, changeset} -> 
         render conn, "new.html", changeset: changeset
     end
