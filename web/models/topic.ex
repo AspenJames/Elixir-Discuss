84883b72b2c5a7f4f24f61e05a65dd312492d7a8
Aspen James <aspenjames@tqca.org>
2018-11-12 22:29:49 -0800

    associate users and topics

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -3,10 +3,12 @@ defmodule Discuss.Topic do
 
   schema "topics" do
     field :title, :string
+
+    belongs_to :user, Discuss.User
   end
 
   def changeset(struct, params \\ %{}) do
-    struct 
+    struct
       |> cast(params, [:title])
       |> validate_required([:title])
   end
